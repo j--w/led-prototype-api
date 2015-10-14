8eeabf15b8d8b2c859bcf23f60c507c512ba77f7
Jim Wright <jim@anow.com>
2015-10-13 21:45:53 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var lights = require('./lights');
+var app = express();
+var app = lights();
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
+
+app.get('/light/:floor/on', function(req, res) {
+  lights.on(req.params.floor);
+  console.log(req.params.floor, 'on');
+  res.send(200);
+});
+
+app.get('/light/:floor/off', function(req, res) {
+  lights.off(req.params.floor);
+  console.log(req.params.floor, 'off');
+  res.send(200);
+});
+
+app.listen(1337);
---
+++ lights.js
@@ -0,0 +1,29 @@
+var makePwm = require( "adafruit-pca9685" );
+var pwm = makePwm();
+
+module.exports = function() {
+  var floorToAddress = {
+    '1': 0,
+    '2': 1,
+    '3': 2,
+    '4': 3,
+    '5': 4
+  };
+
+  return {
+    lightsOn: function(floor) {
+      pwm.setPwm(floorToAddress[floor], 0, 4095);
+    },
+    lightsOff: function(floor) {
+      pwm.setPwm(floorToAddress[floor], 0, 0);
+    },
+    allOn: function() {
+      for(var key in floorToAddress) {
+        pwm.setPwm(floorToAddress[key], 0, 4095);
+      }
+    },
+    allOff: function() {
+      pwm.stop();
+    }
+  };
+};
